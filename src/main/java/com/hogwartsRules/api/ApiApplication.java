2a0f8338c6e3e1a26f866a8a1c2b7fb6cd90d271
2daw3 <2daw3@HZ114497.H012108.NET>
2021-04-26 10:10:31 +0200

    Feat:Tomcat server

--- src/main/java/com/hogwartsRules/api/ApiApplication.java
+++ src/main/java/com/hogwartsRules/api/ApiApplication.java
@@ -4,12 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ApiApplication {
+@RestController
+public class ApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiApplication.class);
+	}
+	@RequestMapping(value="/")
+	public String printStartUpMessage() {
+		return "La api va como dios.";
 	}
 
 }
